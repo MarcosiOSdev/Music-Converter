a68bf97207df89dbde64b1dbc24c1d1b10db6c82
Marcos Felipe <mfelipesp@gmail.com>
2022-02-25 20:03:03 -0300

    Initial Commit

---
+++ Music Converter/Music_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_ConverterApp.swift
+//  Music Converter
+//
+//  Created by marcos.felipe.souza on 25/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Music_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
