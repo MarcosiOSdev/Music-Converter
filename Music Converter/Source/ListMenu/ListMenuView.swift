1cf26e87507464536cc788930b4f80b43caa6dee
Marcos Felipe <mfelipesp@gmail.com>
2022-02-26 19:37:20 -0300

    Add ListMenuView on Stack

---
+++ Music Converter/Source/ListMenu/ListMenuView.swift
@@ -0,0 +1,29 @@
+//
+//  ListMenuView.swift
+//  Music Converter
+//
+//  Created by marcos.felipe.souza on 26/02/22.
+//
+
+import SwiftUI
+
+struct ListMenuView: View {
+    var body: some View {
+        
+        NavigationView {
+            VStack(alignment: .leading, spacing: 8) {
+                NavigationLink("Converter Tablatura para Nota", destination: ConvertTablatureToNoteView())
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Music Converter")
+        }
+    }
+}
+
+struct ListMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListMenuView()
+    }
+}
