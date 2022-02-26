1d4822753ec27d41360609045c40f214b92b590a
Marcos Felipe <mfelipesp@gmail.com>
2022-02-25 23:29:07 -0300

    First Version

--- Music Converter/ContentView.swift
+++ Music Converter/ContentView.swift
@@ -6,12 +6,89 @@
 //
 
 import SwiftUI
+import Combine
+
+class ViewModel: ObservableObject {
+    
+    var notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+    
+    @Published
+    var tablatureNoteSlider: Double = 0
+    
+    @Published
+    var noteInput = ""
+    
+    @Published
+    var tablatureNote: Int = 0
+    
+    @Published
+    var convertedNote: String = ""
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    func onAppear() {
+        $tablatureNoteSlider.combineLatest($noteInput)
+            .filter({
+                self.notes.contains($0.1)
+            })
+            .sink { [weak self] output in
+                guard let self = self else { return }
+                let tablature = output.0
+                let note = output.1
+                
+                let postion = Int(round(tablature))
+                self.tablatureNote = postion
+                self.convertedNote = self.convert(note: note, position: postion)
+                
+            }
+            .store(in: &cancellables)
+    }
+    
+    
+    private func convert(
+        note: String,
+        position: Int
+    ) -> String {
+        
+        if let index = notes.firstIndex(of: note) {
+            let sumNotes = index + position
+            
+            if sumNotes < notes.count {
+                return notes[sumNotes]
+            } else {
+                
+                let resto = sumNotes % notes.count
+                if resto < notes.count {
+                    return notes[resto]
+                }
+            }
+        }
+        return ""
+    }
+}
+
 
 struct ContentView: View {
+        
+    @ObservedObject
+    private var viewModel: ViewModel = ViewModel()
+        
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(alignment: .leading, spacing: 8) {
+            TextField("Nota:", text: $viewModel.noteInput)
+            
+            VStack(alignment: .center) {
+                Slider(value: $viewModel.tablatureNoteSlider, in: 0...24)
+                Text("Na posição: \(viewModel.tablatureNote)")
+            }
+            
+            Text("Nota na escala é: \(viewModel.convertedNote)")
+        }.onAppear(perform: viewModel.onAppear)
     }
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
